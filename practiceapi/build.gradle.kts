ed57ae49ff1de6e3d13a5084393d3ed8ce0b71e7
OiPKL <dhkswls1998@naver.com>
2025-03-20 15:40:20 +0900

    update gradle:api

--- practiceapi/build.gradle.kts
+++ practiceapi/build.gradle.kts
@@ -86,5 +86,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // DataStore 의존성 추가
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
